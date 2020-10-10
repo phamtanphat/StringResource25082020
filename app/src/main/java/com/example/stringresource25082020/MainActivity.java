23645a80c56971f8935c86fac6d6953e46d47a24
phatdroid94 <phatdroid94@gmail.com>
2020-10-10 20:03:50 +0700

    finish

--- app/src/main/java/com/example/stringresource25082020/MainActivity.java
+++ app/src/main/java/com/example/stringresource25082020/MainActivity.java
@@ -2,37 +2,51 @@ package com.example.stringresource25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText mEdtAccount,mEdtPassword;
-    Button mBtnLogin;
-    TextView mTvInfo;
+//    EditText mEdtAccount,mEdtPassword;
+//    Button mBtnLogin;
+//    TextView mTvInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setLocale(this);
         setContentView(R.layout.activity_main);
 
-        mEdtAccount = findViewById(R.id.edittextTaikhoan);
-        mEdtPassword = findViewById(R.id.edittextMatkhau);
-        mBtnLogin = findViewById(R.id.buttonDangnhap);
-        mTvInfo = findViewById(R.id.textviewThongtin);
+//        mEdtAccount = findViewById(R.id.edittextTaikhoan);
+//        mEdtPassword = findViewById(R.id.edittextMatkhau);
+//        mBtnLogin = findViewById(R.id.buttonDangnhap);
+//        mTvInfo = findViewById(R.id.textviewThongtin);
+//
+//        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String account = mEdtAccount.getText().toString();
+//                String password = mEdtPassword.getText().toString();
+//
+//                String textOutputAccount = getResources().getString(R.string.text_output_account);
+//                String textOutputPassword = getResources().getString(R.string.text_output_password);
+//                mTvInfo.setText(String.format("%s : %s \n%s : %s",textOutputAccount,account, textOutputPassword ,password));
+//            }
+//        });
+    }
+    public static void setLocale(Activity context) {
+        Locale locale = new Locale("vi");
+        Configuration config = new Configuration(context.getResources().getConfiguration());
+        Locale.setDefault(locale);
+        config.setLocale(locale);
 
-        mBtnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String account = mEdtAccount.getText().toString();
-                String password = mEdtPassword.getText().toString();
-
-                String textOutputAccount = getResources().getString(R.string.text_output_account);
-                String textOutputPassword = getResources().getString(R.string.text_output_password);
-                mTvInfo.setText(String.format("%s : %s \n%s : %s",textOutputAccount,account, textOutputPassword ,password));
-            }
-        });
+        context.getBaseContext().getResources().updateConfiguration(config,
+                context.getBaseContext().getResources().getDisplayMetrics());
     }
 }
